30c8f611fc875fc929760e2e43a04b1fbb25b91e
Martin Hanzl <martin.hanzl@edookit.com>
2017-03-10 16:59:27 +0100

    code reuse

--- index.php
+++ index.php
@@ -4,13 +4,44 @@ const GOLDEN_NEPAL = 'golden_nepal';
 
 
 const GOLDEN_NEPAL_WEBSITE = 'http://goldennepal.cz/denni-menu/';
-
+const U_BILEHO_BERANKA_WEBSITE = 'http://www.ubilehoberanka.cz/menu-dne';
 
 const MONDAY = 'Pondělí';
 const TUESDAY = 'Úterý';
 const WEDNESDAY = 'Středa';
 const THURSDAY = 'Čtvrtek';
 const FRIDAY = 'Pátek';
+
+const WEEKDAYS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY];
+
+function getRestaurantMeals($website, $dayName, $oneFoodPattern, $startPattern = '')
+{
+    $data = file_get_contents($website);
+
+    $pattern = '~' . $startPattern . $dayName . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . '~i';
+
+    $matches = [];
+
+    preg_match($pattern, $data, $matches);
+
+    return $matches;
+}
+
+function printMealRow($name, $description, $price)
+{
+    echo $name . ' - ' . $description . ' - ' . $price;
+    echo "<br>";
+}
+
+function printRestaurant($name, $data)
+{
+    echo $name;
+    echo "<br>";
+
+    for($i = 1; $i < 16; $i = $i + 3) {
+        printMealRow($data[$i], $data[$i + 1], $data[$i + 2]);
+    }
+}
 
 
 $now = time();
@@ -28,31 +59,22 @@ switch($day) {
     default: break;
 }
 
-$data = file_get_contents(GOLDEN_NEPAL_WEBSITE);
-
-$start = strpos($data, 'pixcode--grid');
-$end = strpos($data, 'pixcode--separator');
-
-$data = substr($data, $start, $end);
-
-$oneFoodPattern = '[\s\S]*?item_title">([^<]+)[\s\S]*?desc__content">([^<]+)[\s\S]*?item-price">([^<]+)';
-
-//$pattern = '~menu-list__title">([^ <]+)[\s\S]*?item_title">([^<]+)~';
-$pattern = '~.*?menu-list__title">' . $dayName . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . '~';
-//$pattern = '~menu-list__title">([^<]+)[^]*?item_title">([^<]+)~im';
-//$pattern = '~pix~m';
-
-$matches = [];
-
-preg_match($pattern, $data, $matches);
-
-echo "Golden Nepal " . $dayName;
-echo "<br>";
-
-for($i = 1; $i < 16; $i = $i + 3) {
-    echo $matches[$i] . ' - ' . $matches[$i + 1] . ' - ' . $matches[$i + 2];
-    echo "<br>";
+if(isset($_GET['day']) && in_array($_GET['day'], WEEKDAYS)) {
+    $dayName = $_GET['day'];
 }
 
-$a = 1;
+foreach (WEEKDAYS as $weekday) {
+    echo "<a href='?day=$weekday'>$weekday</a>";
+}
+echo "<br>";
+echo "<br>";
+
+$matches = getRestaurantMeals(GOLDEN_NEPAL_WEBSITE, $dayName, '[\s\S]*?item_title">([^<]+)[\s\S]*?desc__content">([^<]+)[\s\S]*?item-price">([^<]+)', '.*?menu-list__title">');
+printRestaurant('Golden Nepal', $matches);
+
+$matches = getRestaurantMeals(U_BILEHO_BERANKA_WEBSITE, $dayName, '[\s\S]*?<div class="mc-item">[\s\S]+?mc-text">([^(]+?)\s(\([\d,\s]+\))[\s\S]+?mc-price">([\d\s\S^<]*?)<\/span>[\s\S]*?<\/div>', '<h2>(\S)+.*?[\s\S]+?<\/div>');
+printRestaurant('U Bílého Beránka', $matches);
+
+
+
 
