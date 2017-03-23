d79d95d3f145080da7079867b3f7b5549927ac10
Martin Hanzl <martin.hanzl@edookit.com>
2017-03-23 16:47:00 +0100

    styles and new restaurants

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ const GOLDEN_NEPAL = 'golden_nepal';
 
 const GOLDEN_NEPAL_WEBSITE = 'http://goldennepal.cz/denni-menu/';
 const U_BILEHO_BERANKA_WEBSITE = 'http://www.ubilehoberanka.cz/menu-dne';
+const LIGHT_OF_INDIA_WEBSITE = 'http://www.lightofindia.cz/lang-cs/denni-menu';
 
 const MONDAY = 'Pondělí';
 const TUESDAY = 'Úterý';
@@ -12,10 +13,39 @@ const WEDNESDAY = 'Středa';
 const THURSDAY = 'Čtvrtek';
 const FRIDAY = 'Pátek';
 
-const WEEKDAYS = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY];
+const WEEKDAYS = [
+    1 => [
+      'upper' => 'Pondělí',
+      'lower' => 'pondělí',
+      'regex' => '(pondělí|Pondělí|pondeli|Pondeli|PONDĚLÍ|PONDELI)',
+    ],
+    2 => [
+      'upper' => 'Úterý',
+      'lower' => 'úterý',
+        'regex' => '(úterý|Úterý|utery|Utery|ÚTERÝ|UTERY|Uterý)',
+    ],
+    3 => [
+      'upper' => 'Středa',
+      'lower' => 'středa',
+        'regex' => '(středa|Středa)',
+    ],
+    4 => [
+      'upper' => 'Čtvrtek',
+      'lower' => 'čtvrtek',
+        'regex' => '(čtvrtek|Čtvrtek)',
+    ],
+    5 => [
+      'upper' => 'Pátek',
+      'lower' => 'pátek',
+        'regex' => '(Pátek|patek)',
+    ]
+];
 
-function getRestaurantMeals($website, $dayName, $oneFoodPattern, $startPattern = '')
+function getRestaurantMeals($website, $day, $oneFoodPattern, $startPattern = '')
 {
+
+    $dayName = WEEKDAYS[$day]['regex'];
+
     $data = file_get_contents($website);
 
     $pattern = '~' . $startPattern . $dayName . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . $oneFoodPattern . '~i';
@@ -29,51 +59,103 @@ function getRestaurantMeals($website, $dayName, $oneFoodPattern, $startPattern =
 
 function printMealRow($name, $description, $price)
 {
-    echo $name . ' - ' . $description . ' - ' . $price;
+    echo "<strong>" . $name . '</strong> - ' . $description . ' - ' . $price;
     echo "<br>";
 }
 
 function printRestaurant($name, $data)
 {
-    echo $name;
-    echo "<br>";
+    echo "<div class='restaurant'>" . $name . "</div>";
 
-    for($i = 1; $i < 16; $i = $i + 3) {
-        printMealRow($data[$i], $data[$i + 1], $data[$i + 2]);
+    if(!isset($data[1])) {
+        echo "Data pro tento den nejsou k dispozici.";
+    } else {
+        for ($i = 2; $i < 17; $i = $i + 3) {
+            printMealRow($data[$i], $data[$i + 1], $data[$i + 2]);
+        }
     }
 }
+
+
+echo "
+
+<style>
+    body {
+        font-family: 'Open Sans', Arial, sans-serif;
+    }
+
+    a {
+        text-decoration:none;
+        color:black;
+        font-weight: bold;
+        display: inline-block;
+        padding:5px;
+    }
+    
+    a:hover {
+        color: grey;
+        cursor: pointer;
+    }
+    
+    .selected {
+        text-decoration: underline;
+    }
+    
+    .restaurant {
+        font-size: 120%;
+        font-weight: bold;
+        padding: 20px 0px 10px 0px;
+    }
+    
+    .restaurantsContainer {
+        padding-left: 5px;
+    }
+    
+    .footer {
+        position: absolute;
+        bottom: 10px;
+        left: 5px;
+    }
+
+
+</style>
+
+
+
+";
+
 
 
 $now = time();
 
 $day = date('N', $now);
 
-$dayName = MONDAY;
-
-switch($day) {
-    case 1: $dayName = MONDAY; break;
-    case 2: $dayName = TUESDAY; break;
-    case 3: $dayName = WEDNESDAY; break;
-    case 4: $dayName = THURSDAY; break;
-    case 5: $dayName = FRIDAY; break;
-    default: break;
+if(isset($_GET['day']) && in_array($_GET['day'], [1,2,3,4,5])) {
+    $day = $_GET['day'];
 }
 
-if(isset($_GET['day']) && in_array($_GET['day'], WEEKDAYS)) {
-    $dayName = $_GET['day'];
-}
-
-foreach (WEEKDAYS as $weekday) {
-    echo "<a href='?day=$weekday'>$weekday</a>";
+foreach (WEEKDAYS as $key => $weekday) {
+    if($day == $key) {
+        echo "<a href='?day=$key' class='selected'>" . $weekday['upper'] . "</a>";
+    } else {
+        echo "<a href='?day=$key'>" . $weekday['upper'] . "</a>";
+    }
 }
 echo "<br>";
 echo "<br>";
-
-$matches = getRestaurantMeals(GOLDEN_NEPAL_WEBSITE, $dayName, '[\s\S]*?item_title">([^<]+)[\s\S]*?desc__content">([^<]+)[\s\S]*?item-price">([^<]+)', '.*?menu-list__title">');
+echo "<div class='restaurantsContainer'>";
+$matches = getRestaurantMeals(GOLDEN_NEPAL_WEBSITE, $day, '[\s\S]*?item_title">([^<]+)[\s\S]*?desc__content">([^<]+)[\s\S]*?item-price">([^<]+)', '.*?menu-list__title">');
 printRestaurant('Golden Nepal', $matches);
 
-$matches = getRestaurantMeals(U_BILEHO_BERANKA_WEBSITE, $dayName, '[\s\S]*?<div class="mc-item">[\s\S]+?mc-text">([^(]+?)\s(\([\d,\s]+\))[\s\S]+?mc-price">([\d\s\S^<]*?)<\/span>[\s\S]*?<\/div>', '<h2>(\S)+.*?[\s\S]+?<\/div>');
+$matches = getRestaurantMeals(U_BILEHO_BERANKA_WEBSITE, $day, '[\s\S]*?<div class="mc-item">[\s\S]+?mc-text">([^(]+?)\s(\([\d,\s]+\))[\s\S]+?mc-price">([\d\s\S^<]*?)<\/span>[\s\S]*?<\/div>', '<h2>');
 printRestaurant('U Bílého Beránka', $matches);
+
+$matches = getRestaurantMeals(LIGHT_OF_INDIA_WEBSITE, $day, '[\s\S]*?<br>\S*\s*([\s\S]*?)\s*?(\([\s\S]*?)\s(\d+\s*?Kč)', '<H2>');
+printRestaurant('Light of India', $matches);
+
+echo "</div>";
+
+echo "<div class='footer'>Vytvořil Tíman. Bugy a požadavky na nové restaurace hlaste na timan@centrum.cz</div>";
 
 
 
